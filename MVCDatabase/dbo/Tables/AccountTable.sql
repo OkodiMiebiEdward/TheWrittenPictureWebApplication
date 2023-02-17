6cc10f8d97d9c237170245b60f595b203f822af9
Okodi Miebi Edward <miebiokodiedward@gmail.com>
2023-02-17 10:58:18 -0800

    The SQL SERVER database

---
+++ MVCDatabase/dbo/Tables/AccountTable.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[AccountTable]
+(
+	[Id] INT NOT NULL PRIMARY KEY  IDENTITY(1,1), 
+    [AccountId] INT NOT NULL, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [EmailAddress] NVARCHAR(50) NOT NULL, 
+    [Password] NVARCHAR(50) NOT NULL
+)
